8d0ce89abf1ebe0449406e8688c92ca16f61cd19
fromanint <fromaninteractive@gmail.com>
2016-06-03 09:37:00 -0500

    skills UI

---
+++ Assets/Scripts/UI/Skills/SkillsSel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillsSel : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject[] Skills;
+    int select = 0;
+
+    public void Back(int select) {
+        if (select != 0)
+        {
+
+        }
+    }
+
+}
