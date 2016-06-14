ced65e3fc4d84172f73d0e1cad0ebc0a820bd688
fromanint <fromaninteractive@gmail.com>
2016-06-14 11:05:37 -0500

    Portfolio

---
+++ Assets/Scripts/UI/Portafolio/PortfolioItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class PortfolioItem : MonoBehaviour {
+
+
+    [SerializeField]
+    Image setImange;
+    [SerializeField]
+    Text Description;
+    [SerializeField]
+    Text Name;
+    [SerializeField]
+    Text Url;
+
+
+    Image image;
+    string description;
+    string setname;
+    string url;
+
+    public void InsertItem(Image im, string des, string na, string url)
+    {
+        image = im;
+        description = des;
+        name = na;
+    }
+
+    public void DisplayItem()
+    {
+        setImange = image;
+        Description.text = description;
+        Name.text = name;
+        Url.text = url;
+    }
+
+    public void OpenUrl()
+    {
+        Application.OpenURL(url);
+    }
+
+}
